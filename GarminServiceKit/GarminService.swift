96a4d740652089aa46e82c0150056960dfc6868b
Jan Wrede <wrede.jan@gmail.com>
2024-09-11 13:53:24 +0200

    Skip outdated samples, Send last glucose upon device restore,

--- GarminServiceKit/GarminService.swift
+++ GarminServiceKit/GarminService.swift
@@ -42,26 +42,6 @@ public final class GarminService: Service {
         restoreGarminDevice()
     }
     
-    private func restoreGarminDevice() {
-        //restore previously active device using the uuid from stored user defaults
-        if let savedUUIDString = UserDefaults.standard.string(forKey: "activeGarminDeviceUUID"),
-           let savedUUID = UUID(uuidString: savedUUIDString) {
-            logger.info("Restoring active garmin device from saved UUID: \(savedUUID)")
-            let device = IQDevice(id: savedUUID, modelName: "", friendlyName: "")
-            
-            /*If we don't register the device, sensind messages fails with DeviceNotAvailable
-             We pass nil because we are not actively monitoring device status yet
-             From the documentation: A companion app must register to receive device events before calling methods that
-             operate on devices or apps, such as `getDeviceStatus:` or `sendMessage:toApp:progress:completion:`.
-             */
-            ConnectIQ.sharedInstance().register(forDeviceEvents: device, delegate: nil)
-            setActiveGarminDevice(device)
-            
-        } else {
-            logger.info("No Garmin device information found.")
-        }
-    }
-    
     public var rawState: RawStateValue {
         return [
             "isOnboarded": isOnboarded,
@@ -90,49 +70,87 @@ public final class GarminService: Service {
     }
     
     
-}
-
-extension GarminService: RemoteDataService {
-    public func remoteNotificationWasReceived(_ notification: [String : AnyObject]) async throws {
-        return
+    private func restoreGarminDevice() {
+        //restore previously active device using the uuid from stored user defaults
+        if let savedUUIDString = UserDefaults.standard.string(forKey: "activeGarminDeviceUUID"),
+           let savedUUID = UUID(uuidString: savedUUIDString) {
+            logger.info("Restoring active garmin device from saved UUID: \(savedUUID)")
+            let device = IQDevice(id: savedUUID, modelName: "", friendlyName: "")
+            self.setActiveGarminDevice(device)
+            
+        } else {
+            logger.info("No Garmin device information found.")
+        }
     }
+    
+    public func setActiveGarminDevice(_ device: IQDevice?) {
+
+        //if there was an old device, deregister it
+        if let oldDevice = self.app?.device {
+            self.logger.info("Deregistering for device events for \(oldDevice)")
+            ConnectIQ.sharedInstance().unregister(forDeviceEvents: self.app?.device, delegate: nil)
+            
+            //remove from UserDefaults
+            self.app = nil
+            UserDefaults.standard.removeObject(forKey: "activeGarminDeviceUUID")
+        }
+        
+        self.logger.info("Setting active garmin device to \(device ?? nil)")
+        if let device = device {
+            //TODO: Check if app is installed
+            self.app = IQApp(uuid: UUID(uuidString: "4e32944d-8bbb-41fd-8318-909efae86ac8"), store: UUID(), device: device)
+            // Save the UUID of the device to UserDefaults (or a similar persistent store)
+            UserDefaults.standard.set(device.uuid.uuidString, forKey: "activeGarminDeviceUUID")
+            
+            /*If we don't register the device, sensind messages fails with DeviceNotAvailable
+             We pass nil because we are not actively monitoring device status yet
+             From the documentation: A companion app must register to receive device events before calling methods that operate on devices or apps, such as `getDeviceStatus:` or `sendMessage:toApp:progress:completion:`.
+             */
+            ConnectIQ.sharedInstance().register(forDeviceEvents: device, delegate: nil)
+        }
+        
+        //send with 5 second delay to allow for bluetooth connection
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {self.sendMostRecentGlucose()}
+    }
+
 
     public func sendMessage(_ message: [String: Any]) {
         //We need to send a message to the garmin device using the ConnectIQ framework. We create a message object with the message data and the app object. We then send the message using the ConnectIQ send message method. We also register for the message progress and completion events
         if let app = self.app {
             self.logger.info("Sending message: \(message)")
-            ConnectIQ.sharedInstance().sendMessage(message, to: app, progress: {(sentBytes: UInt32, totalBytes: UInt32) -> Void in
-                let percent: Double = 100.0 * Double(sentBytes / totalBytes)
-                self.logger.debug("Progress: \(percent)% sent \(sentBytes) bytes of \(totalBytes)")
-            }, completion: {(result: IQSendMessageResult) -> Void in
-                self.logger.info("Send message finished with result: \(NSStringFromSendMessageResult(result))")
+            ConnectIQ.sharedInstance().sendMessage(message, to: app, progress: nil, completion: {(result: IQSendMessageResult) -> Void in
+                self.logger.info("Sent message result: \(NSStringFromSendMessageResult(result))")
             })}
         else {
-            self.logger.info("No garmin aplication set, can't send message")
+            self.logger.info("No garmin aplication set, skipping message")
         }
     }
-    
-    public func setActiveGarminDevice(_ device: IQDevice?) {
-        self.logger.info("Setting active garmin device to \(device ?? nil)")
-        //set service app
-        if let device = device {
-            //TODO: Check if app is installed
-            let app = IQApp(uuid: UUID(uuidString: "4e32944d-8bbb-41fd-8318-909efae86ac8"), store: UUID(), device: device)
-            self.app = app!
-        } else {
-            self.app = nil
+
+    private func sendMostRecentGlucose(){
+        let healthStore = HKHealthStore()
+        let glucoseType = HKQuantityType.quantityType(forIdentifier: .bloodGlucose)!
+        let glucoseQuery = HKSampleQuery(sampleType: glucoseType, predicate: nil, limit: 1, sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)]) { (query, samples, error) in
+            if let samples = samples as? [HKQuantitySample] {
+                if let sample = samples.first {
+                    let glucose = sample.quantity.doubleValue(for: HKUnit(from: "mg/dL"))
+                    let date = sample.startDate
+                    let trend = sample.trend?.rawValue ?? -1
+                    let message = ["glucose": glucose, "trend": trend, "timestamp": Int(date.timeIntervalSince1970)] as [String : Any]
+                    
+                    self.logger.debug("Sending most recent glucose: \(glucose) Trend: \(trend) Date: \(date)")
+                    self.sendMessage(message)
+                }
+            }
         }
-        self.logger.debug("Set garmin service app to \(self.app)")
-        
-        // Save the UUID of the device to UserDefaults (or a similar persistent store)
-        if let device = device {
-            UserDefaults.standard.set(device.uuid.uuidString, forKey: "activeGarminDeviceUUID")
-        } else {
-            UserDefaults.standard.removeObject(forKey: "activeGarminDeviceUUID")
-        }
+        healthStore.execute(glucoseQuery)
     }
-    
-    
+}
+
+
+extension GarminService: RemoteDataService {
+    public func remoteNotificationWasReceived(_ notification: [String : AnyObject]) async throws {
+        return
+    }
     
     public func uploadAlertData(_ stored: [LoopKit.SyncAlertObject], completion: @escaping (Result<Bool, any Error>) -> Void) {
         completion(.success(true))
@@ -160,15 +178,19 @@ extension GarminService: RemoteDataService {
     }
     
     public func uploadGlucoseData(_ stored: [LoopKit.StoredGlucoseSample], completion: @escaping (Result<Bool, any Error>) -> Void) {
-        //self.logger.debug("\(Date()):GarminService.uploadGlucoseData \(stored)")
+        
         if !stored.isEmpty{
-            let glucose = stored[0].quantity.doubleValue(for: HKUnit(from: "mg/dL"))
-            let date = stored[0].startDate
-            let trend = stored[0].trend?.rawValue ?? -1
-            self.logger.debug("Sending Glucose: \(glucose) Trend: \(trend) Date: \(date)")
-            
-            let message = ["glucose": glucose, "trend": trend, "timestamp": Int(date.timeIntervalSince1970)] as [String : Any]
-            self.sendMessage(message)
+            //skip outdated samples
+            let sortedSamples = stored.sorted(by: { $0.startDate > $1.startDate })
+            let mostRecentSample = sortedSamples.first!
+            if mostRecentSample.startDate.timeIntervalSinceNow <= 10*60 {
+                let message = ["glucose": mostRecentSample.quantity.doubleValue(for: HKUnit(from: "mg/dL")),
+                               "trend": mostRecentSample.trend?.rawValue ?? -1,
+                               "timestamp": Int(mostRecentSample.startDate.timeIntervalSince1970)] as [String : Any]
+                self.sendMessage(message)
+            } else {
+                self.logger.error("Glucose data is more than 60 seconds old, not sending")
+            }
         }
         completion(.success(true))
         return
